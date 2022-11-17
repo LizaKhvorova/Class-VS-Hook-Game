527c21a3c20ffd9591c677e65f51563c3cc85e29
Khvorova_Liza <liza.khvorova@gmail.com>
2022-11-17 20:16:30 +0300

    Comments added

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import Game from './components/game/game';
-import {GameHook} from './components/game/gameHooks';
+import Game from './components/game/game'; // Add to root.render to watch classes 
+import {GameHook} from './components/game/gameHooks'; // Add to root.render to watch Hooks
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
